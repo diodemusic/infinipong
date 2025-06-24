c975742c6bad6f7c0f222586e4d1db569c8bc4cb
saves <diodemusicuk@gmail.com>
2025-06-24 20:48:12 +0100

    Cap ball speed

--- src/main.lua
+++ src/main.lua
@@ -55,19 +55,9 @@ function lose_life()
     end
 
     if player.lives > 0 then
-
         player.x = 60
-
         enemy.x = 60
-
-        ball.x = 63
-        ball.y = 63
-        ball.x_speed = 0
-
-        if abs(ball.y_speed) ~= 2 do
-            ball.y_speed /= 2
-        end
-
+        reset_ball()
         level_progress = 0
     else
         set_game_over()
@@ -108,10 +98,6 @@ function update_enemy()
 
     enemy.x = mid(0, enemy.x, 120)
     enemy.y = (-ball.y + 54) / 4
-
-    if debug_options.aim_bot then
-        player.x = ball.x - 3
-    end
 end
 
 ------------------- ball
@@ -125,36 +111,50 @@ function update_ball()
     ball.x += ball.x_speed
 
     if ball.x < 1 then
+        ball.x = 1
         ball.x_speed = -ball.x_speed
         sfx(2)
     end
+
     if ball.x > 126 then
+        ball.x = 126
         ball.x_speed = -ball.x_speed
         sfx(2)
     end
 
     if (
-        ball.y >= player.y - 2 and ball.y <= player.y - 1
+        ball.y >= player.y and ball.y <= player.y + 5
     ) and (
-        ball.x >= player.x - 3 and ball.x <= player.x + 9
+        ball.x >= player.x - 2 and ball.x <= player.x + 8
     ) then
-        ball.y_speed = -ball.y_speed
+        ball.y = player.y - 3
+        -- Reverse and clamp y_speed to prevent excessive speed
+        ball.y_speed = -max(1, min(abs(ball.y_speed), ball.y_speed_cap))
         calcuate_ball_x_angle()
         level_progress += 16
         score += 1 * score_multiplier
         sfx(2)
     end
+
     if ball.y <= 17 then
         ball.y_speed = -ball.y_speed
         sfx(2)
     end
+
     if ball.y >= 128 then
         lose_life()
     end
 end
 
 function calcuate_ball_x_angle()
-    ball.x_speed = ((ball.x - player.x) - 3.5) * 0.4
+    ball.x_speed = ((ball.x - player.x) - 3.5) * ball.x_angle_multiplier
+end
+
+function reset_ball()
+    ball.x = player.x + 3
+    ball.y = player.y - 8
+    ball.x_speed = 0
+    ball.y_speed = -abs(ball.y_speed)
 end
 
 ------------------- level
@@ -165,7 +165,10 @@ function update_level()
         score_multiplier += 1
         level_progress = 0
         sfx(0)
-        ball.y_speed *= 1.2
+
+        if ball.y_speed < ball.y_speed_cap then
+            ball.y_speed = abs(ball.y_speed) + ball.y_speed_increase
+        end
     end
 
     player.color = level % #player.colors + 1
@@ -194,7 +197,6 @@ end
 
 function draw_level_progress()
     line(0, 7, level_progress, 7, 7)
-    -- print("ball speed: " .. ball.y_speed, 0, 7, 7)
 end
 
 function update_game_over()
@@ -219,12 +221,15 @@ function draw_pixel_inspector()
         pset(
             debug_options.pixel_inspector.x,
             debug_options.pixel_inspector.y,
-            0
+            11
         )
 
-        print("[" .. debug_options.pixel_inspector.x .. ", " .. debug_options.pixel_inspector.y .. "]",
-        debug_options.pixel_inspector.x + 3,
-        debug_options.pixel_inspector.y - 2)
+        print(
+        "[" .. debug_options.pixel_inspector.x .. ", " .. debug_options.pixel_inspector.y .. "]",
+        96,
+        122,
+            11
+        )
     end
 end
 
@@ -245,35 +250,98 @@ function update_pixel_inspector()
     end
 end
 
+function update_aim_bot()
+    if btnp(4) and debug_options.aim_bot then
+        debug_options.aim_bot_toggle = not debug_options.aim_bot_toggle
+    end
+
+    if debug_options.aim_bot_toggle then
+        player.x = ball.x - 3
+    end
+end
+
+function draw_debug_stats()
+    if not debug_options.show_debug_stats then return end
+
+    local stats = {
+        "p_x: " .. player.x,
+        "p_y: " .. player.y,
+        "p_mov_r: " .. tostr(player.moving_right),
+        "p_mov_l: " .. tostr(player.moving_left),
+        "p_lives: " .. player.lives,
+        "p_move_speed: " .. player.move_speed,
+        "e_x: " .. enemy.x,
+        "e_y: " .. enemy.y,
+        "e_mov_r: " .. tostr(enemy.moving_right),
+        "e_mov_l: " .. tostr(enemy.moving_left),
+        "b_x: " .. ball.x,
+        "b_y: " .. ball.y,
+        "b_x_s: " .. ball.x_speed,
+        "b_y_s: " .. ball.y_speed,
+        "b_y_s_abs: " .. abs(ball.y_speed),
+        "aim_bot: " .. tostr(debug_options.aim_bot),
+        "aim_bot_t: " .. tostr(debug_options.aim_bot_toggle),
+        "game_over: " .. tostr(game_over)
+    }
+    local y = 9
+
+    for i = 1, #stats do
+        print(stats[i], 1, y, 11)
+        y += 6
+    end
+end
+
+function draw_hitboxes()
+    if not debug_options.show_hitboxes then return end
+
+    rect( -- player
+        player.x - 2,
+        player.y,
+        player.x + 8,
+        player.y + 5,
+        11
+    )
+
+    rect( -- ball
+        ball.x,
+        ball.y,
+        ball.x,
+        ball.y,
+        11
+    )
+end
+
 ------------------------------- pico-8 callbacks
 
 function _init()
     music(0, 10, 1)
 
     player = {
-        x=60,
-        y=120,
-        move_speed = 2,
-        colors={0, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-        color=0,
-        color_updated=false,
-        moving_left=false,
-        moving_right=false,
+        x = 60,
+        y = 120,
+        move_speed = 1.5,
+        colors = {0, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+        color = 0,
+        color_updated = false,
+        moving_left = false,
+        moving_right = false,
         lives = 3
     }
     enemy = {
-        x=60,
-        y=8,
-        moving_left=false,
-        moving_right=false
+        x = 60,
+        y = 8,
+        moving_left = false,
+        moving_right = false
     }
     ball = {
-        x=63,
-        y=63,
-        x_speed=0,
-        y_speed=2,
+        x = 63,
+        y = 63,
+        x_speed = 0,
+        y_speed = 2,
+        y_speed_increase = 0.1,
+        y_speed_cap = 4,
+        x_angle_multiplier = 0.4
     }
-
     level = 1
     level_progress = 0
     score = 0
@@ -282,14 +350,17 @@ function _init()
     last_color_change_bounce = 0
 
     debug_options = {
+        show_debug_stats = true,
         pixel_inspect = false,
-        aim_bot = false,
+        aim_bot = true,
+        aim_bot_toggle = false, -- use aim_bot instead
         infinite_lives = false,
         lock_controls = false,
         pixel_inspector = {
             x = 63,
             y = 63
-        }
+        },
+        show_hitboxes = false
     }
 end
 
@@ -302,16 +373,19 @@ function _update()
     update_enemy()
     update_ball()
     update_level()
+    update_aim_bot()
     update_pixel_inspector()
 end
 
 function _draw()
     cls(1)
     map(0, 0)
+    draw_ball()
     draw_enemy()
     draw_stats()
-    draw_ball()
     draw_player()
     draw_game_over()
     draw_pixel_inspector()
+    draw_debug_stats()
+    draw_hitboxes()
 end
